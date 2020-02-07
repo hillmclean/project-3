16905531b3f6cf627dfdbd546c742704913588e6
hilldmc <hilldmc@gmail.com>
2020-02-07 10:24:20 -0800

    NYT API returning data in console

--- js/api.js
+++ js/api.js
@@ -1,1 +1,15 @@
-document.getElementById("test").innerHTML += "this is a test";
+document.addEventListener('DOMContentLoaded', function () {
+
+  const teaserBox = document.getElementsByClassName('teaser');
+
+  teaserBox[0].addEventListener('click', function () {
+    $.ajax({
+      method: 'GET',
+      url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=tGGrhXgAGkkUeGlxub1PQ7smASFvNVgP'
+    })
+
+      .done(function (data) {
+        console.log(data);
+      });
+  });
+}); // end of DOM content load
