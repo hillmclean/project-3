824053b5a90361978ca1db8bf93b5308b3d5d043
hilldmc <hilldmc@gmail.com>
2020-02-07 16:17:57 -0800

    api returns max of 12 artl per ctgy

--- js/api.js
+++ js/api.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const teaserBox = document.getElementsByClassName('teaser');
+  const categories = document.querySelector('.categories');
 
-  teaserBox[0].addEventListener('click', function () {
+  categories.addEventListener('change', () => {
+    let section = event.target.value;
+
     $.ajax({
       method: 'GET',
-      url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=tGGrhXgAGkkUeGlxub1PQ7smASFvNVgP'
+      url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=tGGrhXgAGkkUeGlxub1PQ7smASFvNVgP`
     })
 
       .done(function (data) {
-        data.results.forEach(element => {
-          console.log(element[0, 'abstract']); // returns all abstacts
-        });
+
+        // let areaSelected = 0;
+        for (let i = 0; i < 12; i++) {
+          console.log(data.results[i]);
+        }
+
+        // data.results.forEach(element => {
+        //   console.log(element[0, 'abstract']); // returns all abstacts
+        //   $('.teaser').text(`${element[0, 'abstract']}`);
+        // });
       });
+    // teaser.textContent = `${event.target.value}`;
   });
+
 }); // end of DOM content load
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-const selectElement = document.querySelector('.categories');
+// const categories = document.querySelector('.categories');
+// const result = document.querySelector('.results');
 
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.results');
-  result.textContent = `You like ${event.target.value}`;
-});
+// categories.addEventListener('change', (event) => {
+
+//   $.ajax({
+//     method: 'GET',
+//       url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=tGGrhXgAGkkUeGlxub1PQ7smASFvNVgP'
+//   })
+
+//   .done(function (data) {
+//     data.results.forEach(element => {
+//       console.log(element[0, 'abstract']); // returns all abstacts
+//     });
+//   });
+
+//   result.textContent = `You like ${event.target.value}`;
+// });
